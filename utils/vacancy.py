be3202b17dd61ab601d1035c89b44f0a8c5b8836
nvLast86 <n.lastenko@gmail.com>
2023-03-28 21:29:27 +0600

    The code of class Vacancy has been updated.

--- utils/vacancy.py
+++ utils/vacancy.py
@@ -1,7 +1,14 @@
 class Vacancy:
 
-    def __init__(self, title):
+    def __init__(self, title, area, url,description, salary):
         self.title = title
-        self.url
-        self.descripition
-        self.salary
+        self.area = area
+        self.url = url
+        self.description = description
+        self.salary = salary
+
+    def __repr__(self):
+        return f'{self.title.title()} в городе {self.area.title()}.\nЗарплата: {self.salary}.\n{self.description}\n' \
+               f'Ссылка на вакансию: {self.url}\n'
+
+
