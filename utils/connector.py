0fce6f3929c5aba4dabce2316e073d6659e0d37a
nvLast86 <n.lastenko@gmail.com>
2023-03-31 00:13:49 +0600

    class Connector has been updated

--- utils/connector.py
+++ utils/connector.py
@@ -1,1 +1,26 @@
+from headhunter import HeadHunter
+from superjob import SuperJob
+
+
 class Connector:
+
+    def __init__(self):
+        self.__united_vacancies_list = []
+
+    @property
+    def united_vacancies_list(self):
+        return self.__united_vacancies_list
+
+    @united_vacancies_list.setter
+    def united_vacancies_list(self, object1, object2):
+        self.__united_vacancies_list = object1 + object2
+
+    @united_vacancies_list.getter
+    def united_vacancies_list(self):
+        return self.__united_vacancies_list
+
+if __name__ == '__main__':
+    hh = HeadHunter('python')
+    sj = SuperJob('python')
+    connector = Connector()
+    connector.united_vacancies_list(hh, sj)
