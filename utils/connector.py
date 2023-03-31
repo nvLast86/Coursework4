f94ca2c763e36643a1685cb63c635f06aafcbcc5
nvLast86 <n.lastenko@gmail.com>
2023-03-31 09:44:18 +0600

    united_vacancies_list.setter has been updated

--- utils/connector.py
+++ utils/connector.py
@@ -1,5 +1,5 @@
-from headhunter import HeadHunter
-from superjob import SuperJob
+from utils.headhunter import HeadHunter
+from utils.superjob import SuperJob
 
 
 class Connector:
@@ -12,15 +12,19 @@ class Connector:
         return self.__united_vacancies_list
 
     @united_vacancies_list.setter
-    def united_vacancies_list(self, object1, object2):
-        self.__united_vacancies_list = object1 + object2
+    def united_vacancies_list(self, value):
+        self.__united_vacancies_list = value
 
     @united_vacancies_list.getter
     def united_vacancies_list(self):
         return self.__united_vacancies_list
 
+
 if __name__ == '__main__':
     hh = HeadHunter('python')
+    hh.get_vacancies_list(hh.get_request())
     sj = SuperJob('python')
+    sj.get_vacancies_list(sj.get_request())
     connector = Connector()
-    connector.united_vacancies_list(hh, sj)
+    connector.united_vacancies_list = hh + sj
+    print(connector.united_vacancies_list)
