10362c9c58cd793f6c2a0dfdec6552105f209e92
nvLast86 <n.lastenko@gmail.com>
2023-04-03 01:14:05 +0600

    main.py's code has been added by sample creation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from utils.connector import Connector
 from utils.headhunter import HeadHunter
 from utils.superjob import SuperJob
 
+user_decisions = []
 
 def main():
     # Объясняем для чего данная программа. Далее запрашиваем ключевое слово и производим парсинг
@@ -15,15 +16,35 @@ def main():
     print(f'Список по ключевому слову {user_keyword} создан')
 
     # Создаем текстовый файл со всеми вакансиями по желанию пользователя
-    user_decision = input('Распечатать в файл? (y) - да, остальные нажатия - нет')
+    user_decision = input('Распечатать в файл?\n(y) - да, остальные нажатия - нет\n')
     if user_decision.lower() == 'y':
         connector.print_vacancies_to_file(connector.united_vacancies_list)
         print('Файл создан!\n')
     else:
         print('ok, пропустим\n')
 
-    # Предлагаем создать отдельный файл с топовыми вакансиями
-    print('Может, сформировать топ вакансий по зарплате? (y) - да')
+    # Предлагаем создать отдельный файл с выборкой
+    user_decision = input('Может, сформировать выборку?\n(y) - да, остальные нажатия - нет\n')
+    if user_decision.lower() != 'y':
+        print('ok, пропустим\n')
+    else:
+        user_decision = int(input('От какого размера зарплаты смотреть?\n'))
+        user_decisions.append(int(user_decision))
+        user_decision = int(input('За сколько дней сделать выборку?\n'))
+        user_decisions.append(int(user_decision))
+        user_decision = int(input('Сколькими вакансиями ограничить выборку?\n'))
+        user_decisions.append(int(user_decision))
+        chosen_result = connector.get_chosen_vacancies_list(user_decisions)
+        user_decision = input('Информация сформирована! Распечатать в файл?\n(y) - да, остальные нажатия - нет\n')
+        if user_decision.lower() == 'y':
+            connector.print_vacancies_to_file(chosen_result)
+        else:
+            print("Результаты выборки:\n")
+            print(chosen_result)
+
+
+
+
 
 
 
