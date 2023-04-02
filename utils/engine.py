8413bcf087c049e1c1930775cf7a9e383be95d0f
nvLast86 <n.lastenko@gmail.com>
2023-04-03 02:35:23 +0600

    Docstrings has been added

--- utils/engine.py
+++ utils/engine.py
@@ -2,6 +2,9 @@ from abc import ABC, abstractmethod
 
 
 class Engine(ABC):
+    """
+    Абстрактный класс, от которого наследуют классы HeadHunter и SuperJob
+    """
 
     @abstractmethod
     def get_vacancies(self):
