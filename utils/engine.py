d16e80c281ceb8b0b01f1586f094d2f82ab1af1e
nvLast86 <n.lastenko@gmail.com>
2023-04-02 07:24:08 +0600

    class Engine has been updated

--- utils/engine.py
+++ utils/engine.py
@@ -4,10 +4,5 @@ from abc import ABC, abstractmethod
 class Engine(ABC):
 
     @abstractmethod
-    def get_request(self):
+    def get_vacancies(self):
         pass
-
-    @staticmethod
-    def get_connector(file):
-            """ return the instance of class Connector """
-            pass
