827d4d6c72b90bb3a6cd72b1d7a2e14e42d33a19
nvLast86 <n.lastenko@gmail.com>
2023-04-04 21:29:19 +0600

    docstrings and commentaries have been added to class ServiceBot's methods

--- utils/servicebot.py
+++ utils/servicebot.py
@@ -2,10 +2,14 @@ from datetime import datetime
 
 
 class ServiceBot:
+    """
+    Класс ServiceBot предназначен для взаимодействия с пользователем для создания выборки из списка найденных
+    вакансий на сайтах согласно заданным пользователем критериям по зарплате, дате публикации вакансии и
+    отображения топа вакансий.
+    """
 
-    # def __init__(self, vacancies_list):
-    def __init__(self):
-        self.vacancies_list = []
+    def __init__(self, vacancies_list):
+        self.vacancies_list = vacancies_list
         self.questions = ['От какого размера зарплаты смотреть?\n', 'За сколько дней сделать выборку?\n',
                           'Сколькими вакансиями ограничить выборку?\n']
         self.user_answers = []
@@ -13,15 +17,25 @@ class ServiceBot:
         self.is_correct = False
 
     def __str__(self):
+        """
+        Экземпляр приветствует пользователя, объясняет, для чего он.
+        Если пользователь не желает делать выборку, то бот ничего не делает.
+        """
         if self.is_positive_answer:
             return f'Приветствую! Я специальный бот для работы с полученным списком.\n' \
                    f'Список большой, в нем {len(self.vacancies_list)} вакансий, могу сделать более ' \
                    f'точную выборку.\n'\
                    f'Если нажмешь (y), то я поработаю со списком.\n'
         else:
-            return 'Хорошо, тогда просто выведу список вакансий в файл.\n'
+            return 'Хорошо, ничего не делаю.\n'
 
     def poll_user(self):
+        """
+        Опрос пользователя по критериям, задав ряд вопросов, хранящиеся в поле "questions".
+        Если пользователь вводит не корректные данные, "бот" объяснит, в каком виде он ожидает ввод,
+        и будет требовать до бесконечности корректный ввод. Результаты будут переданы в поле "бота"
+        "user_answers" в виде списка.
+        """
         print('Хорошо! Предварительно задам несколько вопросов для создания выборки.')
         for question in self.questions:
             while True:
@@ -43,6 +57,9 @@ class ServiceBot:
         """
         result = []
 
+        # Проверяем по критерию ожидаемой зарплаты. Если пользователь ввел 0, то пропускаем проверку.
+        # Если ни одна вакансия не подходит, то функция завершает свое действие с уведомлением об
+        # отсутствии подходящих вакансий.
         if user_answers[0] != 0:
             for vacancy in self.vacancies_list:
                 if vacancy.payment[0] > user_answers[0]:
@@ -52,6 +69,9 @@ class ServiceBot:
         if self.have_vacancies(result) is False:
             return 'Увы, вакансий с такой зп нет.'
 
+        # Проверяем по критерию даты публикации. Если пользователь ввел 0, то пропускаем проверку.
+        # Если ни одна вакансия не была в ожидаемый период опубликована, то функция завершает свое действие
+        # с уведомлением об этом.
         if user_answers[1] != 0:
             for vacancy in result:
                 temp_list = vacancy.date_published.split('-')
@@ -62,18 +82,26 @@ class ServiceBot:
         if self.have_vacancies(result) is False:
             return 'Увы, за такой временной промежуток вакансий не появлялось.'
 
+        # Выдаем желаемое количество вакансий. Если вакансий меньше, чем желаемый топ, то выдаем все, что есть.
         if user_answers[2] != 0 or len(result) < user_answers[2]:
             return result
         return result[0:user_answers[2]]
 
     @staticmethod
     def check_answer(answer):
+        """
+        Статистический метод проверки правильности ввода ответа пользователем.
+        Если ответ состоит только из чисел, то ввод корректный.
+        """
         if answer.isdigit():
             return True
         return False
 
     @staticmethod
     def have_vacancies(check_list):
+        """
+        Статистический метод проверки наличия вакансий в списке.
+        """
         if len(check_list) > 0:
             return True
         return False
